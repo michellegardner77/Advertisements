f0b84a2aa707bbc0f023d483f2d13608a61fc470
Michelle Gardner <mgm7c@mail.umkc.edu>
2017-05-02 18:56:46 -0500

    Set up Add Advertisement Scene UI, and created Controller with action methods and field IDs.

---
+++ src/main/controllers/AddAvertisementScene.java
@@ -0,0 +1,20 @@
+package main.controllers;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+
+/**
+ * Created by mgard on 5/2/2017.
+ */
+public class AddAvertisementScene {
+    public TextField addAdvTitleTextField;
+    public TextField addAdvPriceTextField;
+    public TextArea advDetailTextArea;
+    public ComboBox addAdvCatComboBox;
+
+    public void addAdvertismentButtonPressed(ActionEvent actionEvent) {
+
+    }
+}
