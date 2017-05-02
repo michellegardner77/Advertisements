54ae064acae4a66e63264ab3f0f1f23ffd31f97d
Michelle Gardner <mgm7c@mail.umkc.edu>
2017-05-02 18:21:09 -0500

    Set up User Scene UI, and created Controller with action methods and field IDs.

---
+++ src/main/controllers/UserScene.java
@@ -0,0 +1,33 @@
+package main.controllers;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+
+/**
+ * Created by mgard on 5/2/2017.
+ */
+public class UserScene {
+    public TextField titleDescTextField;
+    public ComboBox categoryComBox;
+    public ComboBox periodComBox;
+    public TableView advTable;
+    public TableView myAdvTable;
+
+    public void goButtonPressed(ActionEvent actionEvent) {
+
+    }
+
+    public void editAdvButtonPressed(ActionEvent actionEvent) {
+
+    }
+
+    public void deleteAdvButtonPressed(ActionEvent actionEvent) {
+
+    }
+
+    public void addAdvButtonPressed(ActionEvent actionEvent) {
+        
+    }
+}
